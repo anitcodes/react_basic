dcbf114928f2303c95f464a811e15c5cca82d157
Anit Shrestha <anitshrestha101@gamil.com>
2025-06-15 21:14:31 +0545

    initial commit

---
+++ src/Home.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+    <div>
+      <h1>My name is Anit Shrestha</h1>
+      <h2>I am a frontend developer</h2>
+    </div>
+  )
+}
+
+export default Home
